4ef8df5da32c2a94ad0bd92d8e160f697b7e72ff
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-30 04:01:23 +0000

    Deploy ImpulseAdventure/GUIslice to github.com/ImpulseAdventure/GUIslice.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,46 @@
+var NAVTREE =
+[
+  [ "GUIslice", "index.html", [
+    [ "GUIslice library", "index.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"GUIslice_8c.html",
+"GUIslice__drv__m5stack_8h.html#a1302e252eef8c80542aa9157521555ef",
+"XGauge_8c.html#a873732a73c878d2883f280fcaa199f71",
+"XRadial_8h.html#ae352c66aa5c10dd0af0021461c4b2310",
+"group____ElemCreate__.html#ga8642c1b73d2267081292bc7bf90911f3",
+"group____GraphicsPrimFunc__.html#ga4a098cf3b5cafcc17a37a6ad33d9ff1f",
+"group____Int__.html#ga1b6ee4ea769fdb056b512e7e07feb256",
+"group____Int__.html#gaac0a7c1dee7020cb1e85f613ffc60bf0af77af95fffbfc05fef1dab3baf4b5ba0",
+"group____Touch__.html#ga1a3bb497cd1621393fd8cfa0358aa10d",
+"structgslc__tsXTextbox.html#af205dba9c3902ed6df3fc0191bf79037"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ navtreeindex9.js
@@ -0,0 +1,5 @@
+var NAVTREEINDEX9 =
+{
+"structgslc__tsXTextbox.html#af205dba9c3902ed6df3fc0191bf79037":[3,0,33,6],
+"todo.html":[1]
+};
